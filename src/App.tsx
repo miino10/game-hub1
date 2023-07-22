f4480314c8a1459459ead06cfae1f1cf2b9f6448
ABDIFATAH DAHIR MUSE <abdifataah1619@gmail.com>
2023-07-23 01:14:55 +0300

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,27 @@
 import "./App.css";
-import { Button, ButtonGroup } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
 function App() {
-  return <Button colorScheme="blue">Button</Button>;
+  return (
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav" "nav" "main" "aside"`,
+      }}
+    >
+      <GridItem area="nav" bg="yellow">
+        nav
+      </GridItem>
+      <GridItem area="main" bg="red">
+        main
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gray">
+          Aside
+        </GridItem>
+      </Show>
+    </Grid>
+  );
 }
 
 export default App;
