a16fb27382b382ca2d360c772d50c5a6473ee6ef
ABDIFATAH DAHIR MUSE <abdifataah1619@gmail.com>
2023-07-24 12:57:33 +0300

    Building card games

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -1,5 +1,7 @@
-import { Text } from "@chakra-ui/react";
+import { SimpleGrid, Text } from "@chakra-ui/react";
 import useGames from "../hooks/useGames";
+
+import GameCard from "../components/GameCard";
 
 const GameGrid = () => {
   const { games, error } = useGames();
@@ -7,11 +9,17 @@ const GameGrid = () => {
   return (
     <>
       {error && <Text> {error}</Text>}
-      <ul>
+
+      <SimpleGrid
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
+        spacing={10}
+        padding="10px"
+      >
         {games.map((game) => (
-          <li key={game.id}>{game.name}</li>
+          // <li key={game.id}>{game.name}</li>
+          <GameCard key={game.id} game={game} />
         ))}
-      </ul>
+      </SimpleGrid>
     </>
   );
 };
