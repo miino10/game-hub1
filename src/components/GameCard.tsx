53df515bc10fe11e1945fe1eca5fa18d8f246995
ABDIFATAH DAHIR MUSE <abdifataah1619@gmail.com>
2023-07-24 18:34:05 +0300

    displaying platform icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
-import { Card, CardBody, Heading, Image } from "@chakra-ui/react";
-import React from "react";
+import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
+
 import { Game } from "../hooks/useGames";
+import PaltformIconList from "./PaltformIconList";
 interface Props {
   game: Game;
 }
@@ -11,6 +12,10 @@ const GameCard = ({ game }: Props) => {
       <Image src={game.background_image} />
       <CardBody>
         <Heading fontSize={"2xl"}>{game.name}</Heading>
+
+        <PaltformIconList
+          platforms={game.parent_platforms.map((p) => p.platform)}
+        />
       </CardBody>
     </Card>
   );
