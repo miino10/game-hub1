95684a398664964d2062ccb4e4d0a586c407003c
ABDIFATAH DAHIR MUSE <abdifataah1619@gmail.com>
2023-07-30 19:36:47 +0300

    Ship genres with the app

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,10 +1,11 @@
-import useData from "./useData";
+import genres from "../Data/genres";
+
 export interface Genre {
   id: number;
   name: string;
   image_background: string;
 }
 
-const useGenres = () => useData<Genre>("/genres");
+const useGenres = () => ({ data: genres, isLoding: false, error: null });
 
 export default useGenres;
